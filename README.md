670754a2c9827e4ef26d4fcf294b213812d5c7e0
bryscus <chippylemunk@gmail.com>
2018-09-24 11:44:04 -0400

    Major changes.  Working solenoid timer code.  Still quite a bit kludgey, but seems to work. (+2 squashed commit)
    Squashed commit:
    
    [15a1194] Remove extras from readme
    
    [7888dd8] Cleanup printing and add human readable sunrise and sunset.

--- README.md
+++ README.md
@@ -3,13 +3,8 @@ Controller code for an ESP-01 Relay Module based mist system.  The goal of the p
 configurable system for controlling a mist bed and other irrigation systems.
 
 ## Installation
-### From the Library Manager
-1. Launch the Arduino IDE and navigate to *Sketch → Include Library → Manage Libraries*.
-2. In the library manager, scroll to *Dusk2Dawn* or enter the name into the search field.
-3. Click on the library, then click on the Install button.
-
 ### Requirements
-1. Arduino IDE - tested on v1.6.8
+1. Arduino IDE - tested on v1.8.7
 2. **esp8266** by **ESP8266 Community** library v2.4.2
 3. **Dusk2Dawn** by **DM Kishi** library v1.0.1 - should be installed from ZIP
 
--- mr-mister/mr-mister.ino
+++ mr-mister/mr-mister.ino
@@ -20,13 +20,25 @@
 #include <Dusk2Dawn.h>                  // calculates sunrise/sunset
 
 ////////////////////////////////////////////////////////
+//// ONE TIME CONFIGURATION PARAMETERS /////////////////
 
-#define SSID            "ATT254"
-#define SSIDPWD         "$0n@vation!"
-#define TZ              -5       // (utc+) TZ in hours
-#define DST_MN          60      // use 60mn for summer time in some countries
-#define LAT             26.899078
-#define LON             -80.164002
+#define SSID            "SSID"
+#define SSIDPWD         "Password"
+#define TZ              -5          // (utc+) TZ in hours
+#define DST_MN          60          // use 60mn for summer time in some countries
+#define LAT             26.899078   // Local latitude for sun calculations
+#define LON             -80.164002  // Local longitude for sun calculations
+#define MORN_DELAY_S    0           // Time to wait after sunrise      
+
+////////////////////////////////////////////////////////
+//// TO BE CONVERTED TO RUNTIME ////////////////////////
+
+#define NUM_CHANNELS        1     //Number of channels
+#define CHAN_1_RUNLENGTH_S  10    //Length of spray
+#define CHAN_1_INTERVAL_S   60    //Interval to wait between sprays
+#define CHAN_1_PIN          2     //Pin to drive for channel 1
+#define CHAN_1_EN           LOW   //Enable state
+#define CHAN_1_DIS          HIGH  //Disable state
 
 ////////////////////////////////////////////////////////
 
@@ -48,11 +60,9 @@ void time_is_set(void) {
 void setup() {
   
   Serial.begin(115200);
-  delay(3000);
-  pinMode(0, OUTPUT);
-  pinMode(2, OUTPUT);
-  digitalWrite(0, LOW);
-  digitalWrite(2, LOW);
+  delay(3000);  //Delay to allow catching AP connection in terminal
+  pinMode(CHAN_1_PIN, OUTPUT);
+  digitalWrite(CHAN_1_PIN, CHAN_1_DIS);
 
   settimeofday_cb(time_is_set);
 
@@ -88,60 +98,75 @@ void printTm(const char* what, const tm* tm) {
 
 void printTmShort(const char* what, const tm* tm) {
   Serial.print(what);
-  PTM(year);  PTM(mon);  PTM(mday);
+  PTM(year+1900);  PTM(mon+1);  PTM(mday);
   PTM(hour);  PTM(min);  PTM(sec);
 }
 
-timeval tv;
-timespec tp;
 time_t now, midnight;
+time_t sunrise_t, sunset_t;
 tm *tm;
-uint32_t now_ms, now_us, today_s;
+uint32_t today_s;
 int sunrise_s, sunset_s;
-int today = -1;
+int this_day = -1;
+int runstart_s, starttime_s;
+bool chan_1_msg_flag = true, chan_1_time_flag = true;
+bool five_sec_flag = true;
+uint32_t five_sec_timer = 0;
 
 void loop() {
-
-  // simple drifting loop
-  delay(5000);
-
-  gettimeofday(&tv, nullptr);
-  clock_gettime(0, &tp);
+  //Read time figures
   now = time(nullptr);
-  now_ms = millis();
-  now_us = micros();
-
   tm = localtime(&now);
 
+  //Create five second interval timer for display
+  if((uint32_t)now > five_sec_timer){
+    five_sec_flag = true;
+    five_sec_timer = (uint32_t)now + 5;
+  }
+  
   //Recalculate sunrise/sunset on new day
-  if(today != tm->tm_yday) {
-    sunrise_s  = 60 * location.sunrise(tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, true);
-    sunset_s   = 60 * location.sunset(tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, true);
-    today = tm->tm_yday;
-    Serial.println("Recalculated Sunrise/Sunset");
-
+  if(this_day != tm->tm_yday) {
     uint32_t now_s = (uint32_t)now;
     uint32_t now_hour_s = 3600 * tm->tm_hour;
     uint32_t now_min_s = 60 * tm->tm_min;
     uint32_t now_sec = tm->tm_sec;
-    
-    midnight = (time_t)(now_s - (now_hour_s + now_min_s + now_sec));
-    Serial.print("Midnight: ");
-    Serial.println((uint32_t)midnight);
-    printTmShort("midnight", localtime(&midnight));
-    Serial.println("");
-   }
 
-    today_s = (uint32_t)now - (uint32_t)midnight;
+    //Calculate midnight to create offset for today
+    midnight = (time_t)(now_s - (now_hour_s + now_min_s + now_sec));
+    Serial.print("Midnight:");
+    Serial.print((uint32_t)midnight);
+    printTmShort(" Midnight Time", localtime(&midnight));
+    Serial.println("");
+
+    //Calculate sunrise and sunset
+    sunrise_s  = 60 * location.sunrise(tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, true);
+    sunset_s   = 60 * location.sunset(tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, true);
+    sunrise_t = midnight+(time_t)sunrise_s;
+    sunset_t = midnight+(time_t)sunset_s;
+    Serial.println("Recalculated Sunrise/Sunset");
+    starttime_s = sunrise_s + MORN_DELAY_S;
+
+    //If new boot, set to current time; else, sunrise + offset
+    if(this_day == -1)
+      runstart_s = (uint32_t)now - (uint32_t)midnight;
+    else
+      runstart_s = starttime_s;
+    Serial.print("---- Initial runstart_s :");
+    Serial.println(runstart_s);
+    this_day = tm->tm_yday;
+  }
+
+  //Print human readable time figures every 5 seconds
+  if(five_sec_flag){
     tm = localtime(&now);
 
-    Serial.print("Day of Year: ");
+    Serial.print("DoY:");
     Serial.print(tm->tm_yday);
-    Serial.print(" Day of Month: ");
+    Serial.print(" DoM:");
     Serial.print(tm->tm_mday);
-    Serial.print(" Month of Year: ");
+    Serial.print(" MoY+1:");
     Serial.print(tm->tm_mon+1);
-    Serial.print(" Year: ");
+    Serial.print(" Y+1900:");
     Serial.print(tm->tm_year+1900);
     Serial.print(" Time:");
     Serial.print(tm->tm_hour);
@@ -150,56 +175,54 @@ void loop() {
     Serial.print(":");
     Serial.println(tm->tm_sec);
 
-  //Print calculated sunrise
-  Serial.print("Sunrise:");
-  Serial.print(sunrise_s);
+    //Print calculated sunrise
+    Serial.print("Sunrise:");
+    Serial.print(sunrise_s);
+    printTmShort("   Sunrise", localtime(&sunrise_t));
+    Serial.println();
+    
+    //Print calculated sunset
+    Serial.print("Sunset :");
+    Serial.print(sunset_s);
+    printTmShort("    Sunset", localtime(&sunset_t));
+    Serial.println();
+  }
 
-  //Print calculated sunset
-  Serial.print("  Sunset:");
-  Serial.println(sunset_s);
-  Serial.println();
+  //Calculate the number of seconds so far today
+  today_s = (uint32_t)now - (uint32_t)midnight;
 
-    if((today_s > sunrise_s) && (today_s < sunset_s))
-    {
-       Serial.println("Sun is Up! Today's Seconds: ");
-       Serial.println(today_s);
+  //Guts for running solenoid
+  //Should eventually be a class
+  if((today_s > sunrise_s) && (today_s < sunset_s))
+  {
+    if(chan_1_time_flag){
+      chan_1_time_flag = false;
+      Serial.print("---- Sun is Up! Today's Seconds: ");
+      Serial.println(today_s);
     }
-  // localtime / gmtime every second change
-//  static time_t lastv = 0;
-//  if (lastv != tv.tv_sec) {
-//    lastv = tv.tv_sec;
-//    printTm("localtime", localtime(&now));
-//    Serial.println();
-//    printTm("gmtime   ", gmtime(&now));
-//    Serial.println();
-//    Serial.println();
-//
-//    digitalWrite(0, HIGH);
-//    digitalWrite(2, HIGH);
-//  }
 
-  // time from boot
-  Serial.print("clock:");
-  Serial.print((uint32_t)tp.tv_sec);
-  Serial.print("/");
-  Serial.print((uint32_t)tp.tv_nsec);
-  Serial.print("ns");
+    //Run solenoid during calculated period
+    if((today_s >= runstart_s) && (today_s < (runstart_s + CHAN_1_RUNLENGTH_S))){
+      if(chan_1_msg_flag){
+        chan_1_msg_flag = false;
+        Serial.print("---- Running: ");
+        Serial.println(today_s);
+      }
+      digitalWrite(CHAN_1_PIN, CHAN_1_EN);
+    }
+    else {
+      //Calculate new start time for solenoid
+      if(today_s >= (runstart_s + CHAN_1_RUNLENGTH_S)){
+        chan_1_msg_flag = true;
+        chan_1_time_flag = true;
+        runstart_s += CHAN_1_INTERVAL_S;
+        Serial.print("---- New runstart_s: ");
+        Serial.println(runstart_s);
+      }
+      digitalWrite(CHAN_1_PIN, CHAN_1_DIS);
+    }
+  }
 
-  // EPOCH+tz+dst
-  Serial.print(" gtod:");
-  Serial.print((uint32_t)tv.tv_sec);
-  Serial.print("/");
-  Serial.print((uint32_t)tv.tv_usec);
-  Serial.print("us");
-
-  // EPOCH+tz+dst
-  Serial.print(" time:");
-  Serial.print((uint32_t)now);
-
-  // human readable
-  Serial.print(" ctime:(UTC+");
-  Serial.print((uint32_t)(TZ * 60 + DST_MN));
-  Serial.print("mn)");
-  Serial.print(ctime(&now));
- 
+  //Reset flag
+  five_sec_flag = false;
 }
